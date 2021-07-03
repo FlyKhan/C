f619319a945fa91cba5c01c1cf40a355844719b6
xdotb <liubo666666@outlook.com>
2021-07-03 21:23:17 +0800

    对计算距离问题进行优化

--- 4-1kc.c
+++ 4-1kc.c
@@ -3,10 +3,10 @@
 
 int main(){
     int speed;
-    float lostTime,distance;//花费时间，距离
+    double lostTime,distance;//花费时间，距离
     speed = 40;//40km/h
     lostTime = 1.5;//1h30min
     distance = speed * lostTime;
-    printf("小明家与公司的距离是：%fkm", distance);
+    printf("小明家与公司的距离是：%.2fkm", distance);
     return 0;
 }
