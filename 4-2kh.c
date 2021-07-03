17ac055cfae019a0453b76da457b23a5d7204190
xdotb <liubo666666@outlook.com>
2021-07-03 21:20:58 +0800

    简单的登录验证

---
+++ 4-2kh.c
@@ -0,0 +1,13 @@
+//登录验证
+#include<stdio.h>
+
+int main(){
+    int jskh;//借书卡号
+    char yhxm;//用户姓名
+    printf("请输入借书卡号：");
+    scanf("%d/n", &jskh);
+    printf("请输入用户姓名：");
+    scanf("%c/n", &yhxm);
+    printf("登陆成功");
+    return 0;
+}
