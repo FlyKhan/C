deb61a3b8ef11b34f7bc973c80378818e486432e
xdotb <liubo666666@outlook.com>
2021-07-05 19:03:32 +0800

    求圆柱体积问题V2

--- 5-2kh.c
+++ 5-2kh.c
@@ -1,12 +1,12 @@
 //设一个圆柱底面圆的半径 r=2.5，圆柱的高 h=3，求底面圆周长、圆面积以及圆柱的体积。用 scanf 输入数据，输出计算结果，要求保留小数点后两位。
 #include<stdio.h>
+#define PI 3.1415926
 
 int main(){
-    double r, h;//半径，高
-    double dmyzc, dmymj, yztj;//底面圆周长，底面圆面积，圆柱体积
-    double PI = 3.1415926;
+    float r, h;//半径，高
+    float dmyzc, dmymj, yztj;//底面圆周长，底面圆面积，圆柱体积
     printf("请输入圆柱体的半径和高:\n");
-    scanf("%lf%lf", &r, &h);//两次输入之间间隔一个空格或回车
+    scanf("%f%f", &r, &h);//两次输入之间间隔一个空格或回车
     dmyzc = 2 * PI * r;
     dmymj = PI * r * r;
     yztj = dmymj * h;
