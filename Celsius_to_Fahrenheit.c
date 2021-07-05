7fe86d699847b9129c4d798f5e970e7cb403f8e9
xdotb <liubo666666@outlook.com>
2021-07-05 19:25:13 +0800

    摄氏温度转华氏温度

--- Celsius_to_Fahrenheit.c
+++ Celsius_to_Fahrenheit.c
@@ -10,6 +10,7 @@ int main(int argc, char *argv[])
     if (argc != 2)
         return 1;
     // 注意 argv[0] 是执行的程序，argv[1] 是第1个参数
+    //C 库函数 int atoi(const char *str) 把参数 str 所指向的字符串转换为一个整数（类型为 int 型）。
     input = atoi(argv[1]);
     // TODO：将输入的 input（摄氏温度）值转为华氏温度并打印输出
     float output = 0;
