cd4586894f863ef3668fa772db307308e49a62db
xdotb <liubo666666@outlook.com>
2021-08-10 19:53:19 +0800

    (指针参数方法)指针解决整数排序问题

--- 10-3.c
+++ 10-3.c
@@ -2,6 +2,7 @@
 int main()
 {
     void swap(int *point_1, int *point_2);
+    void swap_2(int x, int y); //对比参照用
     int *p1, *p2, a, b;
 
     printf("Please enter two integer number:");
@@ -10,6 +11,7 @@ int main()
     p2 = &b;
     if (a < b)
     {
+        // swap_2(a,b);//互换结果不会回传，错误用法
         swap(p1, p2);
     }
     // 如果 a < b，那么 a 和 b 的值进行了交换
@@ -28,4 +30,12 @@ void swap(int *point_1, int *point_2)
     temp = *point_1; //使 *p1 和 *p2 互换
     *point_1 = *point_2;
     *point_2 = temp;
+}
+
+void swap_2(int x, int y)
+{
+    int temp;
+    temp = x;
+    x = y;
+    y = temp;
 }
